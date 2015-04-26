b726f327a02d7d1ff1ea03c598c10695e3db2c01
t1ks0n <yusuke.3.tokunaga@gmail.com>
2015-04-26 15:57:13 +0900

    motion土方用の分割

---
+++ TurnLeft.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace KHR_MayFes
+{
+    public partial class MotionManager
+    {
+        private int[] GetTURN_LEFTDests()
+        {
+            int[] ret = { 7500, 7500, 7500, 7530, 7470, 8000, 7000, 8500, 6500, 6900, 8100, 7520, 7480 };
+            return ret;
+        }
+    }
+}
